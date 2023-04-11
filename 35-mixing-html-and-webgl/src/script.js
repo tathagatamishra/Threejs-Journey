4f1a4e56c4154990063592895ed64337110b4e75
Tathagata Mishra <tathagatamishra007@gmail.com>
2023-04-11 13:07:36 +0530

    animated pc

--- 35-mixing-html-and-webgl/src/script.js
+++ 35-mixing-html-and-webgl/src/script.js
@@ -1,44 +1,66 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { gsap } from 'gsap'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import {gsap} from 'gsap'
 
-/**
- * Loaders
- */
-const loadingBarElement = document.querySelector('.loading-bar')
+
+
+
+// Loaders
+
+const loadingElement = document.querySelector('.loading')
+
 const loadingManager = new THREE.LoadingManager(
     // Loaded
-    () =>
-    {
-        // Wait a little
-        window.setTimeout(() =>
-        {
-            // Animate overlay
-            gsap.to(overlayMaterial.uniforms.uAlpha, { duration: 3, value: 0, delay: 1 })
-
-            // Update loadingBarElement
-            loadingBarElement.classList.add('ended')
-            loadingBarElement.style.transform = ''
-        }, 500)
+    () => 
+    { 
+        gsap.delayedCall(0.5, () =>
+            {
+                console.log('loaded'); 
+                gsap.to(overlayMat.uniforms.uAlpha, {duration: 3, value: 0})
+                loadingElement.classList.add('ended')
+                loadingElement.style.transform = ''
+            })
+        // window.setTimeout(() => 
+        // {
+        //     // console.log('loaded'); 
+        //     gsap.to(overlayMat.uniforms.uAlpha, {duration: 3, value: 0})
+        //     loadingElement.classList.add('ended')
+        //     loadingElement.style.transform = ''
+        // }, 500)
+        
     },
 
     // Progress
-    (itemUrl, itemsLoaded, itemsTotal) =>
-    {
-        // Calculate the progress and update the loadingBarElement
-        const progressRatio = itemsLoaded / itemsTotal
-        loadingBarElement.style.transform = `scaleX(${progressRatio})`
+    (itemUrl, itemLoaded, itemTotal) => 
+    { 
+        const loadingRatio = itemLoaded / itemTotal
+        console.log(loadingRatio); 
+        loadingElement.style.transform = `scaleX(${loadingRatio})`
     }
 )
+
 const gltfLoader = new GLTFLoader(loadingManager)
 const cubeTextureLoader = new THREE.CubeTextureLoader(loadingManager)
 
-/**
- * Base
- */
+
+
+
 // Debug
 const debugObject = {}
+
+
+
+
+
+
+
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -46,16 +68,39 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Overlay
- */
-const overlayGeometry = new THREE.PlaneGeometry(2, 2, 1, 1)
-const overlayMaterial = new THREE.ShaderMaterial({
+// Camera
+const camera = new THREE.PerspectiveCamera(55, sizes.width / sizes.height, 0.1, 1000)
+camera.position.set(20, 5, -10)
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true,
+    alpha: true
+})
+
+renderer.physicallyCorrectLights = true
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.toneMapping = THREE.LinearEncoding
+renderer.toneMappingExposure = 1
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+
+
+
+
+// Overlay
+const overlayGeo = new THREE.PlaneGeometry(2, 2, 1, 1)
+const overlayMat = new THREE.ShaderMaterial({
     // wireframe: true,
-    transparent: true,
+    transparent:true,
     uniforms:
     {
-        uAlpha: { value: 1 }
+        uAlpha: {value: 1}
     },
     vertexShader: `
         void main()
@@ -64,20 +109,24 @@ const overlayMaterial = new THREE.ShaderMaterial({
         }
     `,
     fragmentShader: `
-        uniform float uAlpha;
+        uniform float uAlpha;    
 
         void main()
         {
-            gl_FragColor = vec4(0.0, 0.0, 0.0, uAlpha);
+            gl_FragColor = vec4(1, 1, 1, uAlpha);
         }
     `
 })
-const overlay = new THREE.Mesh(overlayGeometry, overlayMaterial)
+
+const overlay = new THREE.Mesh(overlayGeo, overlayMat)
 scene.add(overlay)
 
-/**
- * Update all materials
- */
+
+
+
+
+
+// Update all materials
 const updateAllMaterials = () =>
 {
     scene.traverse((child) =>
@@ -93,9 +142,7 @@ const updateAllMaterials = () =>
     })
 }
 
-/**
- * Environment map
- */
+// Environment map
 const environmentMap = cubeTextureLoader.load([
     '/textures/environmentMaps/0/px.jpg',
     '/textures/environmentMaps/0/nx.jpg',
@@ -107,19 +154,39 @@ const environmentMap = cubeTextureLoader.load([
 
 environmentMap.encoding = THREE.sRGBEncoding
 
-scene.background = environmentMap
+// scene.background = environmentMap
 scene.environment = environmentMap
 
 debugObject.envMapIntensity = 2.5
 
-/**
- * Models
- */
+
+
+
+
+
+// Models
+
+// console.log(DRACOLoader);
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+
+gltfLoader.setDRACOLoader(dracoLoader)
+
+
+let mixer = null
+
 gltfLoader.load(
-    '/models/DamagedHelmet/glTF/DamagedHelmet.gltf',
+    '/models/hololiveen/scene.gltf',
     (gltf) =>
     {
-        gltf.scene.scale.set(2.5, 2.5, 2.5)
+        mixer = new THREE.AnimationMixer(gltf.scene)
+        const action = mixer.clipAction(gltf.animations[0])
+        // console.log(action);
+
+        action.play()
+
+        gltf.scene.scale.set(4, 4, 4)
+        gltf.scene.position.set(0, -4, 0)
         gltf.scene.rotation.y = Math.PI * 0.5
         scene.add(gltf.scene)
 
@@ -127,9 +194,10 @@ gltfLoader.load(
     }
 )
 
-/**
- * Lights
- */
+
+
+// Lights
+
 const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
 directionalLight.castShadow = true
 directionalLight.shadow.camera.far = 15
@@ -138,13 +206,58 @@ directionalLight.shadow.normalBias = 0.05
 directionalLight.position.set(0.25, 3, - 2.25)
 scene.add(directionalLight)
 
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+
+
+
+
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.minDistance = 10;
+controls.maxDistance = 50;
+controls.minPolarAngle = 0;
+controls.maxPolarAngle =  Math.PI * 0.5;
+controls.panSpeed = 0.3
+
+
+
+
+//Animate
+
+const clock = new THREE.Clock()
+let previousTime = 0
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
+
+
+
+    // Update animation mixer
+    if(mixer !== null) {
+
+        mixer.update(deltaTime)
+    }
+
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
 }
+
+tick()
+
+
+
+
 
 window.addEventListener('resize', () =>
 {
@@ -160,48 +273,3 @@ window.addEventListener('resize', () =>
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(4, 1, - 4)
-scene.add(camera)
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true
-})
-renderer.physicallyCorrectLights = true
-renderer.outputEncoding = THREE.sRGBEncoding
-renderer.toneMapping = THREE.ReinhardToneMapping
-renderer.toneMappingExposure = 3
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-/**
- * Animate
- */
-const tick = () =>
-{
-    // Update controls
-    controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-tick()
